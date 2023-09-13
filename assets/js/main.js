25f73dd48616e507becb8bf50c169bd0551f4787
freetimezp <kraftukrnet@ukr.net>
2023-09-13 23:32:07 +0300

    05. js-event-02-wedding. Main. Switch sections.

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,48 @@ window.addEventListener('load', () => {
         });
     });
 });
+
+//activate section 
+const sectionNavigator = name => {
+    let sections = document.querySelectorAll('section');
+    let header = document.querySelector('header');
+    sections.forEach(section => {
+        section.classList.remove('section-show');
+        if (section.classList.contains(name)) {
+            section.classList.add('section-show');
+            header.classList.add('active');
+        }
+    })
+};
+
+window.addEventListener('load', () => {
+    const navList = document.querySelectorAll('.nav-btn');
+    navList.forEach(nav => {
+        nav.addEventListener('click', function (e) {
+            e.preventDefault();
+            navList.forEach(el => {
+                el.classList.remove('active');
+            });
+            this.classList.add('active');
+            sectionNavigator(this.getAttribute('data-target'));
+            screen.width < 768 && toggleMenu();
+        });
+    })
+});
+
+const resetHeader = () => {
+    let header = document.querySelector('header');
+    header.classList.remove('active');
+}
+
+//initial navigation
+const initNavigation = () => {
+    const navList = document.querySelectorAll('.nav-btn');
+    navList.forEach(el => {
+        el.classList.remove('active');
+        if (el.getAttribute('data-target') === 'about') {
+            el.classList.add('active');
+        }
+    });
+    sectionNavigator('about');
+}
